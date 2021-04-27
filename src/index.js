0b376e404efffc3c22ed5a4259fbbfa5219a9f06
Patrick Yambrick <patrickyambrick@gmail.com>
2021-04-27 11:34:20 -0400

    Seed and firebase setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import { FieldValue, firebase } from './lib/firebase';
 
-ReactDOM.render( <App />, document.getElementById('root') );
-
-//client-side rendered app: react (cra)
-  // -> database which is Firebase
-  // -> react-loading-skeleton
-  // tailwind
-// folder structure
-  // src
-    // -> components
-    // -> constants 
-    // -> context
-    // -> helpers 
-    // -> lib (firebase is going to live in here)
-    // -> services (firebase functions in here)
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
